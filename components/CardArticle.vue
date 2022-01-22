816a8c65f19d19e67e3971775f7c3a5f2a51712b
Sergey Bur <burkatsky@ucoz-team.net>
2022-01-22 20:31:18 +0300

    feat(cardArticle): Добавит анимации при наведении на карточку

--- components/CardArticle.vue
+++ components/CardArticle.vue
@@ -1,30 +1,31 @@
 <template>
-  <div class="border bg-white flex flex-col relative m-2 border-[#ededed] overflow-hidden rounded-[15px]">
-    <img
-      class="w-100 h-auto"
-      :src="article.image"
-      :alt="article.title" />
+    <div class="border bg-white m-2 group border-[#ededed] hover:border-gray-500 overflow-hidden rounded-[15px]">
+      <a class="flex flex-col h-full" :href="`/${article.url}`">
+        <img
+          class="w-100 h-auto transition-opacity group-hover:opacity-75"
+          :src="article.image"
+          :alt="article.title" />
 
-    <div class="flex space-x-2 p-4 pb-0">
-      <a href="#">{{ article.category }}</a>
-      <div class="dot-before">
-        {{ correctDate }}
-      </div>
+        <div class="flex space-x-2 p-4 pb-0">
+          <a href="#" class="transition-opacity hover:opacity-50">{{ article.category }}</a>
+          <div class="dot-before">
+            {{ correctDate }}
+          </div>
+        </div>
+
+        <div class="p-4 pt-1 pb-2">
+          <h1 class="text-xl font-semibold">{{ article.title }}</h1>
+        </div>
+
+        <div class="flex flex-col justify-end px-4 pt-2 pb-4 h-full">
+          <div class="flex space-x-2 items-center">
+            <ArticleLike :article="article"/>
+            <IconComment class="header-icon"/>
+            <IconBookmark class="header-icon"/>
+          </div>
+        </div>
+      </a>
     </div>
-
-    <div class="p-4 pt-1 pb-2">
-      <h1 class="text-xl font-semibold">{{ article.title }}</h1>
-    </div>
-
-    <div class="flex flex-col justify-end px-4 pt-2 pb-4 h-full">
-      <div class="flex space-x-2">
-        <ArticleLike :article="article"/>
-        <IconComment class="header-icon"/>
-        <IconBookmark class="header-icon"/>
-      </div>
-    </div>
-
-  </div>
 </template>
 
 <script>
