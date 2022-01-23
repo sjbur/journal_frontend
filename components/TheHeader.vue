62a8ab9f964b54930c08aa5ce67c9150f037e15e
Sergey Bur <burkatsky@ucoz-team.net>
2022-01-23 22:18:54 +0300

    feat(TheHeader): Сделает шапку sticky и задаст ей цвет фона

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,14 +1,15 @@
 <template>
-  <div class="p-6 border-b border-gray-200 flex justify-between">
+  <div class="p-6 border-b border-gray-200 flex justify-between sticky top-0 bg-body">
     <TheLogo/>
 
     <div class="flex items-center space-x-6">
-      <IconSearch />
-      <IconBell />
-      <button class="btn-black"  @click="$auth.loggedIn? $router.push({ path: '/add-post' }) : ''">Написать</button>
+      <IconSearch/>
+      <IconBell/>
+      <button class="btn-black" @click="$auth.loggedIn? $router.push({ path: '/add-post' }) : ''">Написать</button>
 
-      <IconUser v-if="!$auth.loggedIn" @click="$nuxt.$emit('showLogin')" />
-      <img v-else width="35" height="35" class="cursor-pointer" src="/no-avatar.png" alt=""  @click="$router.push({ path: '/profile' }) "/>
+      <IconUser v-if="!$auth.loggedIn" @click="$nuxt.$emit('showLogin')"/>
+      <img v-else width="35" height="35" class="cursor-pointer" src="/no-avatar.png" alt=""
+           @click="$router.push({ path: '/profile' }) "/>
     </div>
   </div>
 </template>
@@ -17,14 +18,10 @@
 import IconSearch from "~/components/icons/IconSearch";
 import IconBell from "~/components/icons/IconBell";
 import IconUser from "~/components/icons/IconUser";
+
 export default {
   name: "TheHeader",
   components: {IconBell, IconSearch, IconUser},
-  methods: {
-    emitLogin() {
-
-    }
-  }
 }
 </script>
 
